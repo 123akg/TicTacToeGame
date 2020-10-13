d1e3339f6fb83ed79f0d49a04475983f34091413
123akg <abhijeetkumargiri7070@gmail.com>
2020-10-13 21:53:46 +0530

    Added: TTT Board Created

--- src/com/capgemini/workshop/tictactoegame/TicTacToeGame.java
+++ src/com/capgemini/workshop/tictactoegame/TicTacToeGame.java
@@ -2,8 +2,16 @@ package com.capgemini.workshop.tictactoegame;
 
 public class TicTacToeGame {
 	
+	static char[] BOARD;
 	public static void main(String[] args) {
-		System.out.println("Welcome to TicTacToe Game");
+		System.out.println("Welcome to TicTacToe game");
+		createBoard();
+	}
+	public static void createBoard() {
+		BOARD= new char[10];
+		for(int i=1;i<10;i++) {
+			BOARD[i] = ' ';
+		}
 	}
 
 }
